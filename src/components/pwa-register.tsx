afcf31861622c83093d0ae7bf5ce6c39c6c9a96f
Z User <z@container>
2026-07-10 16:18:55 +0000

    274c764f-4a0c-47c7-9996-fbe391839c8e

--- src/components/pwa-register.tsx
+++ src/components/pwa-register.tsx
@@ -8,7 +8,7 @@ export function PWARegister() {
       navigator.serviceWorker
         .register('/sw.js')
         .then((reg) => {
-          console.log('PWA: Service Worker registered, version v15');
+          console.log('PWA: Service Worker registered, version v16');
 
           // Listen for new SW taking over, then force reload once
           let refreshing = false;
